3b1cbcd23acb23bdb62ac181331c22fcd65d7c8e
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-02 11:15:29 +0100

    flat params edited

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -29,7 +29,7 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(:flat).permit(:name, :picture)
+    params.require(:flat).permit(:title, :price_per_day, :capacity, :dexcription, :type)
   end
 end
 
