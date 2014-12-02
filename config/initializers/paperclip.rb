7435c06e4299b0638bec6398626f00f98cfedfc9
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-02 15:10:03 +0100

    accolade ajoutée dans paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,3 +5,4 @@ Paperclip::Attachment.default_options[:s3_credentials] = {
   bucket:             ENV['S3_BUCKET_NAME'],        # \
   access_key_id:      ENV['AWS_ACCESS_KEY_ID'],     #  |- DO NOT replace this
   secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']  # /
+}
