c317130fdd7ff3f0f0d72dd3f0de1bad8974e72d
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-02 11:10:39 +0100

    added add flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,6 @@
 class FlatsController < ApplicationController
   before_action :authenticate_user!
-  efore_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show]
 
   def index
     @flats = Flat.all
@@ -9,9 +9,27 @@ class FlatsController < ApplicationController
   def show
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
   end
+
+  def flat_params
+    params.require(:flat).permit(:name, :picture)
+  end
 end
+
