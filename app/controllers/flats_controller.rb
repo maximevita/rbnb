f9b1ee466fa30ada401480b9feab63354adb896d
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-02 12:27:56 +0100

    in flat  : updating a flat is working

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,6 @@
 class FlatsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :edit, :update]
 
   def index
     @flats = Flat.all
@@ -16,6 +16,17 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
       redirect_to flat_path(@flat)
     else
       render :new
