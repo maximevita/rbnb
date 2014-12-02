59a2074bc43dd4e11570f1ba88eac4dca0413f7a
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-02 16:35:11 +0100

    moving to new branch

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,26 @@
 class PicturesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_flat, only: [:create]
+
+  def new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:flat_image)
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :flat
+  has_attached_file :flat_image,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :flat_image,
+    content_type: /\Aimage\/.*\z/
 end
