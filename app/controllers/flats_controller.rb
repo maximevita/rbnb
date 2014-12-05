60fbe156151ec7f39d8c2cf6aa1f182ed746f847
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-12-05 12:44:21 +0100

    updating flat view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -68,7 +68,7 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(:flat).permit(:title, :price_per_day, :capacity, :description, :housing_type)
+    params.require(:flat).permit(:title, :address, :price_per_day, :capacity, :description, :housing_type)
   end
 
   def search_params
